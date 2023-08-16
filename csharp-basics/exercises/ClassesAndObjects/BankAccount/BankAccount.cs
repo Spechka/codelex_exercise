1bc7d83fcdf5d68d63da22433082dc2745a37a16
Spechka <anrio53@inbox.lv>
2023-08-16 14:44:39 +0300

    ClassesAndObjectsFix2

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class BankAccount
+    {
+        private string _name;
+        private double _balance;
+
+        public BankAccount(string name, double balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            string balanceFormat = (_balance >= 0) ? $"${_balance:F2}" : $"-${Math.Abs(_balance):F2}";
+            return $"{_name}, {balanceFormat}";
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -5,25 +5,11 @@ namespace BankAccount
 {
     class Program
     {
-        public static string Username { get; set; }
-
-        public static double Balance { get; set; }
-
         static void Main(string[] args)
         {
-            Username = "Benson";
-            Balance = -17.2;
-            Console.WriteLine(ShowUserNameAndBalance());
-        }
-
-        public static string ShowUserNameAndBalance()
-        {
-            if (Balance < 0)
-            {
-                return $"{Username}, -${Math.Abs(Balance):F2}";
-            }
-
-            return $"{Username}, ${Balance:F2}";
+            BankAccount benben = new BankAccount("Benson", 17.5);
+            string result = benben.ShowUserNameAndBalance();
+            Console.WriteLine(result);
         }
     }
 }
